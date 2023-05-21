fedf2870e8bfebd9c09862f66c0ec545a44dcda0
Gonçalo <a95212@alunos.uminho.pt>
2023-05-21 04:24:19 +0100

    Avanços

--- Projeto0/JS/localStorage.js
+++ Projeto0/JS/localStorage.js
@@ -15,33 +15,38 @@ var postar = [{
 var postarcoluna = [{
     "Tipo": "Animais",
     "Descricao": "Os animais precisam dos nossos cuidados e proteção, cria um futuro mais seguro e feliz para eles.",
-    "ref": "Detalhe.html"
+    
 }, {
     "Tipo": "Educação",
     "Descricao": "O voluntariado na educação é importante para contribuir para uma sociedade mais justa.",
-    "ref": "referencia"
+    
 }, {
     "Tipo": "Meio ambiente",
     "Descricao": "És preocupado com o ambiente? Faz a tua parte e ajuda-nos a preservar o meio ambiente.",
-    "ref": "referencia"
+    
 }, {
     "Tipo": "Turismo social",
     "Descricao": "Gostas de viajar? Agarra na oportunidade de explorar novos lugares e ajudar pessoas.",
-    "ref": "referencia"
+    
 }, {
     "Tipo": "Auxílio de doentes",
     "Descricao": "Faça a diferença na vida de alguém, ajude a trazer conforto e apoio para aqueles que mais precisam.",
-    "ref": "referencia"
+    
 }, {
     "Tipo": "Desporto",
     "Descricao": "És apaixonado pelo desporto? Esta é a tua oportunidade de ajudar a construir uma comunidade mais ativa.",
-    "ref": "referencia"
+   
 }];
 
 var imagemdetalhe = [{
-    "imagem": "./images/animais.jpg"
+    "imagem": "./images/animais.jpg",
+    "Tipo": "Animais"
 },{
-    "imagem": "./images/educacao.jpeg"
+    "imagem": "./images/educacao.jpeg",
+    "Tipo": "Educação"
+},{
+    "imagem": "./images/animais.jpg",
+    "Tipo": "Animais"
 }];
 
 var itensdetalhe = [{
@@ -52,7 +57,7 @@ var itensdetalhe = [{
     "Data": " 24-11-2002",
     "Local": " Viana do Castelo",
     "VagasDisponiveis": " 25",
-
+    "Tipo": "Animais",
     "Contacto": " 987654321"
 
 },{
@@ -63,8 +68,19 @@ var itensdetalhe = [{
     "Data": " 23-04-2003",
     "Local": " Lisboa",
     "VagasDisponiveis": " 20",
-
+    "Tipo": "Educação",
     "Contacto": " 987654321"
+
+},{
+
+
+    "Descricaoo": "Durante a campanha de voluntariado curta, trabalharemos em parceria com abrigos e organizações de resgate de animais locais. Nosso foco principal será oferecer assistência e suporte aos animais que estão em busca de um lar amoroso. Haverá várias atividades gratificantes nas quais você poderá participar, desde passear com cães, brincar com gatos, ajudar nos cuidados diários até auxiliar em eventos de adoção.",
+    "Organizacao": "Animal Crossing",
+    "Data": " 24-11-2002",
+    "Local": " Viana do Castelo",
+    "VagasDisponiveis": " 25",
+    "Tipo": "Animais",
+    "Contacto": " 987654320"
 
 }];
 
