b5fcc8dcb1671f5e21f62f66534f665842705a85
Chalau200 <132441207+Chalau200@users.noreply.github.com>
2023-05-26 10:36:29 +0100

    teste

--- JS/localStorage.js
+++ JS/localStorage.js
@@ -160,9 +160,9 @@ localStorage.setItem("ImagensOportunidades", JSON.stringify(postar))
 
 localStorage.setItem("Oportunidades", JSON.stringify(postarcoluna))
 
-localStorage.setItem("ImagemDetalhe", JSON.stringify(imagemdetalhe))
+//localStorage.setItem("ImagemDetalhe", JSON.stringify(imagemdetalhe))
 
-localStorage.setItem("ItensDetalhe", JSON.stringify(itensdetalhe))
+//localStorage.setItem("ItensDetalhe", JSON.stringify(itensdetalhe))
 
 localStorage.setItem("ImagensDoaçoes", JSON.stringify(postarDoaçoes))
 
