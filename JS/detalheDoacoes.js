766f4cb35e81d7c5dfff2e0326e95b82e7122e53
Chalau200 <132441207+Chalau200@users.noreply.github.com>
2023-05-27 20:05:37 +0100

    tentativa

--- JS/detalheDoacoes.js
+++ JS/detalheDoacoes.js
@@ -7,50 +7,93 @@ let colunaHTMLDetalhesDoacoes = '';
 const TipoDoacaoSelecionado = JSON.parse(localStorage.getItem("TipoEscolhido1"));
 const containerDetalhesDoacoes = document.getElementById("postcolumnDetalhesDoacoes");
 
+
+
 // Recupera as informações da última doação armazenada
 const ultimaDoacao = JSON.parse(localStorage.getItem('UltimaDoacao'));
 
-// Recupera a lista de doações do usuário do localStorage
-const listaDoacoes = JSON.parse(localStorage.getItem('listaDoacoes')) || [];
+// Obtém o valor total de doações armazenado no localStorage para o tipo selecionado
+let totalDinheiroDoado = parseFloat(localStorage.getItem(`TotalDinheiroDoado_${TipoDoacaoSelecionado}`)) || 0;
+
+// Obtém o valor máximo para o tipo de doação selecionado
+const valorMaximo = postcolunaDetalhesDoacoes.find(post => post.TipoDoacoes === TipoDoacaoSelecionado).valorMaximo;
+
+
 
 if (TipoDoacaoSelecionado) {
     const imagensDetalhesDoacoes = postsDetalhesDoacoes.filter(postDetalhesDoacoes => postDetalhesDoacoes.TipoDoacoes === TipoDoacaoSelecionado);
     const textoDetalheDoacoes = postcolunaDetalhesDoacoes.filter(postDetalhesDoacoes => postDetalhesDoacoes.TipoDoacoes === TipoDoacaoSelecionado);
 
-    let totalDinheiroDoado = 0;
-    const valorMaximo = 20; // Defina o valor máximo desejado
+    // Define o valor máximo para cada tipo de doação no objeto postcolunaDetalhesDoacoes
+    postcolunaDetalhesDoacoes.forEach(post => {
+        if (post.TipoDoacoes === "Medicamentos") {
+            post.valorMaximo = 200; // Defina o valor máximo para o Tipo1 como 200
+        } else if (post.TipoDoacoes === "Casas Seguras") {
+            post.valorMaximo = 300; // Defina o valor máximo para o Tipo2 como 300
+        } else if (post.TipoDoacoes === "Cuidar Animais") {
+            post.valorMaximo = 400; // Defina o valor máximo para o Tipo3 como 150
+        }
+        else if (post.TipoDoacoes === "Roupas") {
+            post.valorMaximo = 500; // Defina o valor máximo para o Tipo3 como 150
+        }
+        else if (post.TipoDoacoes === "Ensinar") {
+            post.valorMaximo = 600; // Defina o valor máximo para o Tipo3 como 150
+        }
+        else if (post.TipoDoacoes === "Comida Animais") {
+            post.valorMaximo = 700; // Defina o valor máximo para o Tipo3 como 150
+        }
+    });
+
+    const valorMaximo = postcolunaDetalhesDoacoes.find(post => post.TipoDoacoes === TipoDoacaoSelecionado).valorMaximo;
+
 
     for (let i = 0; i < textoDetalheDoacoes.length; i++) {
-        const postElementDetalhesDoacoes = document.createElement('div');
-        postElementDetalhesDoacoes.classList.add("cartao1");
-        postElementDetalhesDoacoes.innerHTML = '<div id="' + textoDetalheDoacoes[i].Instituicao + '" class="cartaodinamico">' +
-            '<div id="c" class="card2">' +
-            '<div class="row g-0">' +
-            '<div class="col-8 col-md-5">' +
-            '<img src="' + imagensDetalhesDoacoes[i].imagemDoacoes + '" style="width:auto; height:auto; padding-right:20px" ' +
-            'class="card-img img-fluid rounded-start" alt="Categoria" />' +
-            '</div>' +
-            '<div class="col-4 col-md-7">' +
-            '<div id="body-detalhe" class="card-body d-flex flex-column">' +
-            '<div class="h-100">' +
-            '<h3 id="titulo" class="card-title">' + textoDetalheDoacoes[i].titulo + '</h3>' +
-            '<p id="desc" class="card-desc">' + textoDetalheDoacoes[i].DescricaoDoacoes + '</p>' +
-            '<p id="tipo" class="card-desc">' + textoDetalheDoacoes[i].TipoDoacoes + '</p>' +
-            '<p id="Instituicao" class="card-title">' + textoDetalheDoacoes[i].Instituicao + '</p>' +
-            '<p id="multibanco" class="card-desc">' + textoDetalheDoacoes[i].ReferenciaMultibanco + '</p>' +
-            '<p id="ContactoInstituicao" class="card-desc">' + '<i class="bi bi bi-telephone icon-orange">' + '</i>' + textoDetalheDoacoes[i].ContactoInstituicao + '</p>' +
-            '<div class="form-group">' +
-            '<label for="valorDoacao">Valor da doação</label>' +
-            '<input type="number" id="valorDoacao' + i + '" class="form-control" placeholder="Insira o valor da doação">' +
-            '</div>' +
-            '<p>' + '<a id="' + textoDetalheDoacoes[i].ContactoInstituicao + '" class="btn btn-primary marcar"> Doar </a></p>' +
-            '</div>' +
-            '</div>' +
-            '</div>' +
-            '</div>' +
-            '</div>' +
-            '</div>';
-        containerDetalhesDoacoes.appendChild(postElementDetalhesDoacoes);
+        // Verifica se o tipo de doação atual é igual ao tipo selecionado pelo usuário
+        if (textoDetalheDoacoes[i].TipoDoacoes === TipoDoacaoSelecionado) {
+            const postElementDetalhesDoacoes = document.createElement('div');
+            postElementDetalhesDoacoes.classList.add("cartao1");
+            postElementDetalhesDoacoes.innerHTML = '<div id="' + textoDetalheDoacoes[i].Instituicao + '" class="cartaodinamico">' +
+                '<div id="c" class="card2DetalhesDoacoes">' +
+                '<div class="row g-0">' +
+                '<div class="col-8 col-md-5">' +
+                '<img src="' + imagensDetalhesDoacoes[i].imagemDoacoes + '" style="width:auto; height:auto; padding-right:20px" ' +
+                'class="card-img img-fluid rounded-start" alt="Categoria" />' +
+                '</div>' +
+                '<div class="col-4 col-md-7">' +
+                '<div id="body-detalhe" class="card-body d-flex flex-column">' +
+                '<div class="h-100">' +
+                '<h3 id="tituloDetalheDoacoes" class="card-title">' + textoDetalheDoacoes[i].titulo + '</h3>' +
+                '<p id="tipo" class="card-desc">' + textoDetalheDoacoes[i].TipoDoacoes + '</p>' +
+                '<p id="descDetalheDoacoes" class="card-desc">' + textoDetalheDoacoes[i].DescricaoDoacoes + '</p>' +
+                '<p id="Instituicao" class="card-title">' + '<i class="bi bi-chat-square-heart icon-orange"></i> ' + textoDetalheDoacoes[i].Instituicao + '</p>' +
+                '<p id="multibanco" class="card-desc">' + '<i class="bi bi-cash-coin icon-orange"></i> ' + textoDetalheDoacoes[i].ReferenciaMultibanco + '</p>' +
+                '<p id="ContactoInstituicao" class="card-desc">' + '<i class="bi bi bi-telephone icon-orange"> ' + '</i>' + textoDetalheDoacoes[i].ContactoInstituicao + '</p>' +
+                '<div id="valorDoacoes" class="form-group">' +
+                '<label for="valorDoacao">Valor da doação (Máximo: ' + valorMaximo + ')</label>' +
+                '<input type="number" id="valorDoacao' + i + '" class="form-control" placeholder="Insira o valor da doação">' +
+                '</div>' +
+                '<p>' + '<a id="' + textoDetalheDoacoes[i].ContactoInstituicao + '" class="btn btn-primary marcar"> Doar </a></p>' +
+                '<div id="progress-bar-container">' +
+                '<div id="progress-bar"></div>' +
+                '</div>' +
+                '</div>' +
+                '</div>' +
+                '</div>' +
+                '</div>' +
+                '</div>';
+            containerDetalhesDoacoes.appendChild(postElementDetalhesDoacoes);
+        }
+    }
+
+    const progressBar = document.getElementById('progress-bar');
+    const progress = (totalDinheiroDoado / valorMaximo) * 100;
+
+    if (progress <= 100) {
+        progressBar.style.width = progress + '%';
+        progressBar.innerText = progress.toFixed(1) + '%';
+    } else {
+        progressBar.style.width = '100%';
+        progressBar.innerText = '100%';
     }
 
     const btnDoar = document.querySelectorAll(".marcar");
@@ -74,25 +117,36 @@ if (TipoDoacaoSelecionado) {
                     };
                     localStorage.setItem('UltimaDoacao', JSON.stringify(doacao));
 
-                    // Adiciona o valor doado ao total
+                    // Adiciona o valor doado ao total específico para o tipo selecionado
                     totalDinheiroDoado += Number(valorDoacao);
 
-                    // Verifica se o valor máximo foi atingido
+                    // Atualiza a barra de progresso específica para o tipo selecionado
+                    const progress = (totalDinheiroDoado / valorMaximo) * 100;
+
+                    if (progress <= 100) {
+                        progressBar.style.width = progress + '%';
+                        progressBar.innerText = progress.toFixed(1) + '%';
+                    } else {
+                        progressBar.style.width = '100%';
+                        progressBar.innerText = '100%';
+                    }
+
+                    // Verifica se o valor máximo foi atingido para o tipo selecionado
                     if (totalDinheiroDoado > valorMaximo) {
                         alert("O valor máximo de doação para esta categoria foi atingido.");
                         // Você pode adicionar código adicional aqui para lidar com a situação quando o valor máximo é atingido
                     } else {
-                        // Adiciona a doação à lista de doações do usuário
-                        listaDoacoes.push(doacao);
-
-                        // Armazena a lista de doações atualizada no localStorage
-                        localStorage.setItem('listaDoacoes', JSON.stringify(listaDoacoes));
+                        // Salva o novo valor total de doações específico para o tipo selecionado no localStorage
+                        localStorage.setItem(`TotalDinheiroDoado_${TipoDoacaoSelecionado}`, totalDinheiroDoado.toString());
 
                         // Exibe as informações da última doação no console
                         console.log('Última doação:');
                         console.log('Valor: ' + doacao.valor);
                         console.log('Utilizador: ' + doacao.utilizador);
                         console.log('Instituição: ' + doacao.instituicao);
+                        console.log('Valor total das doações para o tipo ' + TipoDoacaoSelecionado + ': ' + totalDinheiroDoado);
+
+
 
                         // Processar a doação
                         alert('Doação efetuada com sucesso!');
@@ -100,18 +154,9 @@ if (TipoDoacaoSelecionado) {
                 }
             } else {
                 alert('Você precisa fazer login primeiro.');
-                //window.location.href = "Login.html";
+                //window.location.href = 'login.html'; // Redireciona para a página de login
             }
         });
     });
-
-    const infoDoacao = document.getElementById("infoDoacao");
-
-    if (ultimaDoacao) {
-        infoDoacao.innerHTML = 'Última doação: <br>' +
-            'Valor: ' + ultimaDoacao.valor + '<br>' +
-            'Utilizador: ' + ultimaDoacao.utilizador + '<br>' +
-            'Instituição: ' + ultimaDoacao.instituicao;
-    }
+    console.log("Máximo que pode doar: " + valorMaximo);
 }
-
--- JS/localStorage.js
+++ JS/localStorage.js
@@ -40,7 +40,7 @@ var postarcoluna = [{
 
 
 
-/*DOAÇÕES*/
+/DOAÇÕES/
 var postarDoaçoes = [{
     "imagemDoacoes": "./images/campanha1.png"
 }, {
@@ -56,98 +56,103 @@ var postarDoaçoes = [{
 }];
 
 var postarcolunaDoaçoes = [{
-    "TipoDoacoes": "Categoria 1",
-    "DescricaoDoacoes": "Garantir medicamentos para todas as crianças.",
+    "TipoDoacoes": "Medicamentos",
+    "DescricaoDoacoes": "Garantir medicamentos para todas as crianças é uma ação crucial para promover a justiça social e para um futuro saudável para cada indivíduo na sociedade.",
     "refDoacoes": "referencia"
 }, {
-    "TipoDoacoes": "Categoria 2",
-    "DescricaoDoacoes": "Garantir casas seguras para as crianças.",
+    "TipoDoacoes": "Casas Seguras",
+    "DescricaoDoacoes": "Garantir casas seguras para as crianças é uma responsabilidade coletiva, proporciona um ambiente protetor e acolhedor para um desenvolvimento feliz.",
     "refDoacoes": "referencia"
 }, {
-    "TipoDoacoes": "Categoria 3",
-    "DescricaoDoacoes": "Assistência de Animais",
+    "TipoDoacoes": "Cuidar Animais",
+    "DescricaoDoacoes": "Garantir assistência aos animais é essencial para promover seu bem-estar e respeitar sua dignidade como seres vivos.",
     "refDoacoes": "referencia"
 }, {
-    "TipoDoacoes": "Categoria 4",
-    "DescricaoDoacoes": "Assegurar roupa para todas as crianças.",
+    "TipoDoacoes": "Roupas",
+    "DescricaoDoacoes": "Assegurar roupas para todas as crianças é fundamental para garantir seu conforto, dignidade e proteção contra as condições climáticas adversas.",
     "refDoacoes": "referencia"
 }, {
-    "TipoDoacoes": "Categoria 5",
-    "DescricaoDoacoes": "Aulas de reforço escolar",
+    "TipoDoacoes": "Ensinar",
+    "DescricaoDoacoes": "Oferecer aulas de reforço escolar ajuda a promover a igualdade educacional e garantir que todas as crianças possam alcançar o seu potencial académico.",
     "refDoacoes": "referencia"
 }, {
-    "TipoDoacoes": "Categoria 6",
-    "DescricaoDoacoes": "Doação de ração para animais.",
+    "TipoDoacoes": "Comida Animais",
+    "DescricaoDoacoes": "A doação de ração para animais é uma ação que visa acabar com as necessidades alimentares destes seres indefesos, contribuindo para sua saúde e bem-estar.",
     "refDoacoes": "referencia",
 
 
 }];
 
-/*Detalhe Doações*/
+/Detalhe Doações/
 var imagensDetalheDoacoes = [{
     "imagemDoacoes": "./images/campanha1.png",
-    "TipoDoacoes": "Categoria 1"
+    "TipoDoacoes": "Medicamentos"
 }, {
     "imagemDoacoes": "./images/campanha2.png",
-    "TipoDoacoes": "Categoria 2"
+    "TipoDoacoes": "Casas Seguras"
 }, {
     "imagemDoacoes": "./images/campanha3.png",
-    "TipoDoacoes": "Categoria 3"
+    "TipoDoacoes": "Cuidar Animais"
 }, {
     "imagemDoacoes": "./images/campanha4.png",
-    "TipoDoacoes": "Categoria 4"
+    "TipoDoacoes": "Roupas"
 }, {
     "imagemDoacoes": "./images/campanha5.png",
-    "TipoDoacoes": "Categoria 5"
+    "TipoDoacoes": "Ensinar"
 }, {
     "imagemDoacoes": "./images/campanha6.png",
-    "TipoDoacoes": "Categoria 6"
+    "TipoDoacoes": "Comida Animais"
 }];
 
 var postarDetalheDoacoes = [{
     "titulo": "Garantir medicamentos",
-    "TipoDoacoes": "Categoria 1",
-    "Instituicao": "Ajuda",
+    "TipoDoacoes": "Medicamentos",
+    "Instituicao": "Med Kids",
     "ContactoInstituicao": "939840430",
     "ReferenciaMultibanco": "12345678",
-    "DescricaoDoacoes": "Garantir medicamentos para todas as crianças.",
+    "DescricaoDoacoes": "Garantir o acesso a medicamentos para todas as crianças é um objetivo essencial para promover sua saúde e bem-estar. Crianças são vulneráveis a uma série de doenças e condições de saúde, e é nosso dever assegurar que elas recebam os medicamentos necessários para o tratamento adequado.",
+    
 
 }, {
-    "titulo": "Garantir medicamentos",
-    "TipoDoacoes": "Categoria 2",
-    "Instituicao": "Ajuda",
+    "titulo": "Garantir casas seguras",
+    "TipoDoacoes": "Casas Seguras",
+    "Instituicao": "Safe Home",
     "ContactoInstituicao": "939840430",
     "ReferenciaMultibanco": "12345678",
-    "DescricaoDoacoes": "Garantir medicamentos para todas as crianças.",
+    "DescricaoDoacoes": "Garantir casas seguras para as crianças é uma prioridade essencial para proteger o seu bem-estar e desenvolvimento saudável. Cada criança merece um lar seguro, livre de riscos e ameaças à sua segurança física e emocional. Isso implica construir e manter casas adequadas, livres de perigos como instalações elétricas precárias, janelas desprotegidas e outros potenciais pontos de acidentes.",
+   
 }, {
-    "titulo": "Garantir medicamentos",
-    "TipoDoacoes": "Categoria 3",
-    "Instituicao": "Ajuda",
+    "titulo": "Garantir assistência aos animais",
+    "TipoDoacoes": "Cuidar Animais",
+    "Instituicao": "Help Pets",
     "ContactoInstituicao": "939840430",
     "ReferenciaMultibanco": "12345678",
-    "DescricaoDoacoes": "Garantir medicamentos para todas as crianças.",
+    "DescricaoDoacoes": "Garantir assistência adequada aos animais é um compromisso essencial para promover o seu bem-estar e respeitar a sua dignidade enquanto seres vivos. Isso inclui fornecer cuidados veterinários regulares, alimentação adequada, um abrigo seguro e oportunidades de exercício e interação social.",
+  
 }, {
-    "titulo": "Garantir medicamentos",
-    "TipoDoacoes": "Categoria 4",
-    "Instituicao": "Ajuda",
+    "titulo": "Assegurar roupas para todas as crianças",
+    "TipoDoacoes": "Roupas",
+    "Instituicao": "Clothes for kids",
     "ContactoInstituicao": "939840430",
     "ReferenciaMultibanco": "12345678",
-    "DescricaoDoacoes": "Garantir medicamentos para todas as crianças.",
-
+    "DescricaoDoacoes": "Assegurar roupas para todas as crianças é uma responsabilidade social crucial para promover a dignidade e o bem-estar de cada criança. Cada criança merece ter acesso a roupas adequadas para se proteger do frio e do calor, além de promover a sua autoestima e integração social. Isso requer um esforço coletivo para fornecer roupas de qualidade, confortáveis e adequadas ao clima, considerando as necessidades de cada criança.",
+   
 }, {
-    "titulo": "Garantir medicamentos",
-    "TipoDoacoes": "Categoria 5",
-    "Instituicao": "Ajuda",
+    "titulo": "Oferecer aulas de reforço escolar",
+    "TipoDoacoes": "Ensinar",
+    "Instituicao": "School Help",
     "ContactoInstituicao": "939840430",
     "ReferenciaMultibanco": "12345678",
-    "DescricaoDoacoes": "Garantir medicamentos para todas as crianças.",
+    "DescricaoDoacoes": "Oferecer aulas de reforço escolar é uma medida valiosa para promover a igualdade de oportunidades educacionais e apoiar o sucesso académico de todas as crianças. Nem todas as crianças têm o mesmo acesso a recursos educacionais. Ao disponibilizar aulas de reforço, podemos ajudar essas crianças a superar dificuldades, preencher lacunas de conhecimento e fortalecer as suas habilidades académicas.",
+    
 }, {
-    "titulo": "Garantir medicamentos",
-    "TipoDoacoes": "Categoria 6",
-    "Instituicao": "Ajuda",
+    "titulo": "Doação de ração para animais",
+    "TipoDoacoes": "Comida Animais",
+    "Instituicao": "Food for Animals",
     "ContactoInstituicao": "939840430",
     "ReferenciaMultibanco": "12345678",
-    "DescricaoDoacoes": "Garantir medicamentos para todas as crianças.",
+    "DescricaoDoacoes": "A doação de ração para animais é uma atitude solidária que visa acabar com as necessidades nutricionais dos animais em situação de vulnerabilidade. Muitos animais de estimação ou em abrigos dependem exclusivamente de doações para receberem alimentação adequada.",
+    
 
 }];
 
@@ -172,6 +177,62 @@ var postarGaleria = [{
 }, {
     "imagemGaleria": "./images/galeria9.png"
 }];
+
+
+/*Sobre nós*/
+var imagensSobreNos = [{
+    "imagemNOS": "./images/HOMEM.jpeg",
+},
+ {
+    "imagemNOS": "./images/mulher1.jpg",
+}, {
+    "imagemNOS": "./images/mulher2.jpg",
+}, {
+    "imagemNOS": "./images/anonimo.jpg",
+}, {
+    "imagemNOS": "./images/anonimo.jpg",
+}, {
+    "imagemNOS": "./images/menina-adolescente.jpg",
+}];
+
+var postarSobreNos = [{
+    "Nome": "João Abreu",
+    "Descricao": "Fazer parte desta causa ajudou me a encontrar sentido na vida.",
+    "Galeria": "OCG-Salvar o Oceano",
+    "Tipo": "Voluntário",
+
+},
+ {
+    "Nome": "Sónia Santos",
+    "Descricao": "Senti-me bem em ajudar e ensinar a desenvolver a comunidade.",
+    "Galeria": "Galeria",
+    "Tipo": "Voluntária"
+
+}, {
+    "Nome": "Ana Silva",
+    "Descricao": "Com a HelpMates ajudei crianças a serem mais felizes.",
+    "Galeria": "Galeria",
+    "Tipo": "Voluntária"
+
+}, {
+    "Nome": "Anónimo",
+    "Descricao": "Graças à HelpMates consegui ter um Natal feliz pela primeira vez."
+    }, {
+    "Nome": "Anónimo",
+    "Descricao": "A HelpMates permitiu que vários animais pudessem voltar a ter uma casa.",
+    "Galeria": "Galeria",
+    "Tipo": "Beneficiário"
+
+}, {
+    "Nome": "Paula Sá",
+    "Descricao": "Com a ajuda da HelpMates consegui melhorar a minha vida.",
+    "Galeria": "Galeria",
+    "Tipo": "Beneficiária"
+}];
+
+localStorage.setItem("ImagensSobreNos", JSON.stringify(imagensSobreNos))
+
+localStorage.setItem("SobreNos", JSON.stringify(postarSobreNos))
 
 localStorage.setItem("Galeria", JSON.stringify(postarGaleria))
 
