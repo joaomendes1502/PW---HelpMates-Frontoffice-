f745f9b535e29436db13664cd2874ef75de33c81
ricardormpereira <132283886+ricardormpereira@users.noreply.github.com>
2023-05-31 00:47:36 +0100

    TRADUTOR

--- JS/tradutor.js
+++ JS/tradutor.js
@@ -2,7 +2,7 @@ window.gtranslateSettings = {
     "default_language": "pt",
     "native_language_names": true,
     "detect_browser_language": true,
-    "languages": ["pt", "fr", "de", "es", "en", "zh-CN"],
+    "languages": ["pt", "en"],
     "wrapper_selector": ".gtranslate_wrapper",
     "float_switcher_open_direction": "bottom",
     "flag_style": "3d"
